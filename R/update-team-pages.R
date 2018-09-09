55ffbdfe413c600284e330585edc0e9d29fe26aa
David Whiting <david@davidwhiting.me.uk>
2018-09-09 15:09:26 +0100

    Added comments

--- R/update-team-pages.R
+++ R/update-team-pages.R
@@ -1,22 +1,25 @@
 ## Script to convert online format of teams data to structure needed 
 ## for the website
 
+## Set working directory to source location
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
 library(googlesheets)
 
+## Link to the spreadsheet
 x <- gs_title("2018/19")
+## Get the teams sheet from the database
 db <- gs_read(x, ws = "teams")
 
+## Get the csv file from the website
 ws <- read.csv("../_data/teams.csv")
 
-names(db)
-names(ws)
-
 ## Create two columns we need
-db$training <- paste(db$trainingVenue, db$trainingDay, db$trainingTime)
+db$training <- paste(db$trainingVenue, db$trainingDay, db$trainingTime, sep = ", ")
 db$name <- db$Team
 
 ## Check for missing names
-names(ws)[!names(ws) %in% names(db)]
+stopifnot(length(names(ws)[!names(ws) %in% names(db)]) == 0)
 
 xx <- db[, names(ws)]
 
@@ -26,12 +29,12 @@ for (j in 1:ncol(xx)) {
   xx[i, j] <- ""
 }
 
-
+## Drop rows without a home ground (these will be spurious rows)
 i <- xx$homeground == ''
 xx <- xx[!i, ]
 
-i <- xx$training == "None NA NA"
+## Clean up the training info
+i <- xx$training == "None, NA, NA"
 xx$training[i] <- ""
-
 
 write.csv(xx, file = "../_data/teams.csv", row.names = FALSE)
